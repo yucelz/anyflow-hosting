412620ff961f76a294e19a675082df86197d1bec
yucelz <yucelz@gmail.com>
2025-08-23 13:16:25 +0100

    ++ terraform

---
+++ gcp-terraform/modules/gke/main.tf
@@ -0,0 +1,239 @@
+# GKE module for N8N deployment
+
+# Service account for GKE nodes
+resource "google_service_account" "gke_node_service_account" {
+  account_id   = "${var.cluster_name}-gke-sa"
+  display_name = "GKE Node Service Account for ${var.cluster_name}"
+  description  = "Service account for GKE nodes in ${var.cluster_name} cluster"
+}
+
+# IAM bindings for the service account
+resource "google_project_iam_member" "gke_node_service_account_roles" {
+  for_each = toset([
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter",
+    "roles/monitoring.viewer",
+    "roles/stackdriver.resourceMetadata.writer"
+  ])
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.gke_node_service_account.email}"
+}
+
+# Workload Identity service account (if enabled)
+resource "google_service_account" "workload_identity_service_account" {
+  count = var.enable_workload_identity ? 1 : 0
+
+  account_id   = "${var.cluster_name}-wi-sa"
+  display_name = "Workload Identity Service Account for ${var.cluster_name}"
+  description  = "Service account for Workload Identity in ${var.cluster_name} cluster"
+}
+
+# Create GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.zone
+
+  network    = var.network_name
+  subnetwork = var.subnet_name
+
+  # Remove default node pool
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # Kubernetes version
+  min_master_version = var.kubernetes_version == "latest" ? null : var.kubernetes_version
+
+  # Network configuration
+  ip_allocation_policy {
+    cluster_secondary_range_name  = var.pods_range_name
+    services_secondary_range_name = var.services_range_name
+  }
+
+  # Enable network policy if specified
+  dynamic "network_policy" {
+    for_each = var.enable_network_policy ? [1] : []
+    content {
+      enabled  = true
+      provider = "CALICO"
+    }
+  }
+
+  # Enable workload identity if specified
+  dynamic "workload_identity_config" {
+    for_each = var.enable_workload_identity ? [1] : []
+    content {
+      workload_pool = "${var.project_id}.svc.id.goog"
+    }
+  }
+
+  # Master authorized networks
+  dynamic "master_authorized_networks_config" {
+    for_each = length(var.authorized_networks) > 0 ? [1] : []
+    content {
+      dynamic "cidr_blocks" {
+        for_each = var.authorized_networks
+        content {
+          cidr_block   = cidr_blocks.value.cidr_block
+          display_name = cidr_blocks.value.display_name
+        }
+      }
+    }
+  }
+
+  # Addons configuration
+  addons_config {
+    http_load_balancing {
+      disabled = false
+    }
+
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+
+    network_policy_config {
+      disabled = !var.enable_network_policy
+    }
+
+    dns_cache_config {
+      enabled = true
+    }
+  }
+
+  # Enable logging and monitoring
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+  # Cluster autoscaling
+  cluster_autoscaling {
+    enabled = true
+    resource_limits {
+      resource_type = "cpu"
+      minimum       = 1
+      maximum       = 100
+    }
+    resource_limits {
+      resource_type = "memory"
+      minimum       = 2
+      maximum       = 1000
+    }
+  }
+
+  # Maintenance window
+  maintenance_policy {
+    daily_maintenance_window {
+      start_time = "02:00"
+    }
+  }
+
+  # Enable Shielded GKE nodes
+  node_config {
+    shielded_instance_config {
+      enable_secure_boot          = true
+      enable_integrity_monitoring = true
+    }
+  }
+
+  # Resource labels
+  resource_labels = var.labels
+
+  lifecycle {
+    ignore_changes = [
+      node_config,
+      initial_node_count
+    ]
+  }
+}
+
+# Create node pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = var.node_pool_name
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  
+  # Auto-scaling configuration
+  autoscaling {
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
+
+  # Initial node count
+  initial_node_count = var.node_count
+
+  # Node management
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  # Upgrade settings
+  upgrade_settings {
+    max_surge       = 1
+    max_unavailable = 0
+  }
+
+  # Node configuration
+  node_config {
+    preemptible  = var.preemptible_nodes
+    machine_type = var.machine_type
+    disk_size_gb = var.disk_size_gb
+    disk_type    = var.disk_type
+    image_type   = "COS_CONTAINERD"
+
+    # Service account
+    service_account = google_service_account.gke_node_service_account.email
+
+    # OAuth scopes
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    # Labels and tags
+    labels = merge(var.labels, {
+      cluster = var.cluster_name
+      pool    = var.node_pool_name
+    })
+
+    tags = ["gke-node", var.cluster_name, "n8n"]
+
+    # Shielded instance configuration
+    shielded_instance_config {
+      enable_secure_boot          = true
+      enable_integrity_monitoring = true
+    }
+
+    # Workload metadata configuration for Workload Identity
+    dynamic "workload_metadata_config" {
+      for_each = var.enable_workload_identity ? [1] : []
+      content {
+        mode = "GKE_METADATA"
+      }
+    }
+
+    # Metadata
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+
+  # Network configuration
+  network_config {
+    pod_range     = var.pods_range_name
+    pod_ipv4_cidr_block = null
+  }
+
+  depends_on = [google_container_cluster.primary]
+}
+
+# Bind Workload Identity (if enabled)
+resource "google_service_account_iam_binding" "workload_identity_binding" {
+  count = var.enable_workload_identity ? 1 : 0
+
+  service_account_id = google_service_account.workload_identity_service_account[0].name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "serviceAccount:${var.project_id}.svc.id.goog[${var.workload_identity_namespace}/${var.workload_identity_ksa_name}]",
+  ]
+}
---
+++ gcp-terraform/modules/gke/variables.tf
@@ -0,0 +1,133 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the GKE cluster"
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "The Kubernetes version for the cluster"
+  type        = string
+  default     = "latest"
+}
+
+variable "network_name" {
+  description = "Name of the VPC network"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "pods_range_name" {
+  description = "Name of the pods IP range"
+  type        = string
+}
+
+variable "services_range_name" {
+  description = "Name of the services IP range"
+  type        = string
+}
+
+# Node pool configuration
+variable "node_pool_name" {
+  description = "Name of the node pool"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Initial number of nodes in the node pool"
+  type        = number
+  default     = 1
+}
+
+variable "min_node_count" {
+  description = "Minimum number of nodes in the node pool"
+  type        = number
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "Maximum number of nodes in the node pool"
+  type        = number
+  default     = 3
+}
+
+variable "machine_type" {
+  description = "Machine type for the cluster nodes"
+  type        = string
+  default     = "e2-standard-2"
+}
+
+variable "disk_size_gb" {
+  description = "Disk size in GB for cluster nodes"
+  type        = number
+  default     = 50
+}
+
+variable "disk_type" {
+  description = "Disk type for cluster nodes"
+  type        = string
+  default     = "pd-standard"
+}
+
+variable "preemptible_nodes" {
+  description = "Whether to use preemptible nodes"
+  type        = bool
+  default     = false
+}
+
+# Security configuration
+variable "enable_network_policy" {
+  description = "Enable Kubernetes network policies"
+  type        = bool
+  default     = true
+}
+
+variable "enable_workload_identity" {
+  description = "Enable GKE Workload Identity"
+  type        = bool
+  default     = true
+}
+
+variable "workload_identity_namespace" {
+  description = "Kubernetes namespace for workload identity"
+  type        = string
+  default     = "n8n"
+}
+
+variable "workload_identity_ksa_name" {
+  description = "Kubernetes service account name for workload identity"
+  type        = string
+  default     = "n8n-ksa"
+}
+
+variable "authorized_networks" {
+  description = "List of authorized networks for GKE master"
+  type = list(object({
+    cidr_block   = string
+    display_name = string
+  }))
+  default = []
+}
+
+variable "labels" {
+  description = "Labels to apply to resources"
+  type        = map(string)
+  default     = {}
+}
